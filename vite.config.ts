72f3a5c62348ad87063af4e0434534b6984ca8ed
Fabio Rosada <r.fabio95@gmail.com>
2023-08-27 14:59:01 +0200

    added vite instead of basic react app

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  define: {
+    global: {},
+  },
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  build: {
+    outDir: "dist",
+    minify: "esbuild",
+    target: "esnext",
+  },
+});
