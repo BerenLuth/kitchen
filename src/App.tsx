8b55a367d8901156f4cfd8ea37fcee823acbb9d7
Fabio Rosada <r.fabio95@gmail.com>
2023-08-27 18:51:09 +0200

    added useless homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Recipe from "./pages/recipe";
 import ListRecipes from "./pages/ListRecipes";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Box sx={{ display: "flex", height: "100vh", width: "100vw" }}>
         <Box component="main" sx={{ flexGrow: 1, padding: 0 }}>
           <Routes>
-            <Route path="/" element={<ListRecipes />} />
+            <Route path="/" element={<Home />} />
             <Route path="/recipes" element={<ListRecipes />} />
             <Route path="/recipes/:id" element={<Recipe />} />
           </Routes>
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./styles.css";
+
+const Home: React.FC = () => {
+  const nav = useNavigate();
+
+  return (
+    <div>
+      <h1>Tante bele ricete yeee</h1>
+      <a onClick={() => nav("/recipes")}>Ricettine</a>
+    </div>
+  );
+};
+
+export default Home;
